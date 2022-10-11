c5ec06758f6ac8bb52a2f0e8f80f4d549ce09cc1
MrMad <80827394+MadExploits@users.noreply.github.com>
2022-10-11 18:45:25 +0700

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -24,7 +24,7 @@ echo '
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>403 Forbidden</title>
+    <title>MADUPLOADER</title>
     <style>
         * {
             font-family: monospace;
